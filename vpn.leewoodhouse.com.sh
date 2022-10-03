2b0ff5789cbeb819d22deed64f838982fe1b9e0a
Lee Woodhouse <lee@lab.leewoodhouse.com>
2022-10-03 08:15:54 +0000

    Update vpn.leewoodhouse.com.sh, wild.leewoodhouse.com.sh

--- vpn.leewoodhouse.com.sh
+++ vpn.leewoodhouse.com.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-## change to "bin/sh" when necessary
 
-auth_email="lpwoodhouse@gmail.com"                                      # The email used to login 'https://dash.cloudflare.com'
+auth_email="lpwoodhouse@gmail.com"                  # The email used to login 'https://dash.cloudflare.com'
 auth_method="global"                                # Set to "global" for Global API Key or "token" for Scoped API Token
-auth_key="6228478a2a1fb9ce09407e693b0bd979ac6f7"                                        # Your API Token or Global API Key
-zone_identifier="e010c68bd727a67de05d9be4cdbcb838"                                 # Can be found in the "Overview" tab of your domain
-record_name="vpn.leewoodhouse.com"                                     # Which record you want to be synced
-proxy=false                                        # Set the proxy to true or false
-
-
+auth_key="6228478a2a1fb9ce09407e693b0bd979ac6f7"    # Your API Token or Global API Key
+zone_identifier="e010c68bd727a67de05d9be4cdbcb838"  # Can be found in the "Overview" tab of your domain
+record_name="vpn.leewoodhouse.com"                  # Which record you want to be synced
+proxy=false                                         # Set the proxy to true or false
 
 ###########################################
 ## Check if we have a public IP
